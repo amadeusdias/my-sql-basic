2a751f88b7b756a17a4ae3d0b617fc24aa47f4bb
amadeus <amadeusdias@hotmail.com>
2022-11-02 18:52:43 -0300

    correções

--- desafio23.sql
+++ desafio23.sql
@@ -1,3 +1,3 @@
 UPDATE northwind.order_details
 SET order_details = 30
-WHERE unit_price > 10.000;
+WHERE unit_price < 10.000;
--- desafio24.sql
+++ desafio24.sql
@@ -1,4 +1,4 @@
 UPDATE northwind.order_details
-SET order_details = 30
+SET order_details = 45
 WHERE unit_price > 10.000
 AND id BETWEEN 30 AND 40;
